4a23aabbec0595fec2019c3fdf6d288c0d278f33
nadinelk <nadinekurnadi@gmail.com>
2021-07-23 10:26:57 +0700

    base exercises done

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// num of characters
+// var main = function (input) {
+//   var counter = 0;
+//   var myOutputValue = "";
+//   while (counter < input) {
+//     emojis = myOutputValue + "👍";
+//     myOutputValue = emojis;
+//     counter = counter + 1;
+//   }
+//   return myOutputValue;
+// };
+
+// square
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var numOfRows = 0;
+  var myOutputValue = "";
+  while (numOfRows < input) {
+    var counter = 0;
+    while (counter < input) {
+      emojis = myOutputValue + "👍";
+      myOutputValue = emojis;
+      counter = counter + 1;
+    }
+    myOutputValue = myOutputValue + "<br>";
+    numOfRows = numOfRows + 1;
+  }
   return myOutputValue;
 };
